6e6d39bc63ef5d69a228501867ed83e89e82c904
Lucas Barbosa Barreto <lucasbarbosabarreto551@gmail.com>
2024-10-26 14:12:03 -0300

    sssg

--- src/sistema_de_monitoramento_ambiental_1/pkg0/ProjetoAmbiental.java
+++ src/sistema_de_monitoramento_ambiental_1/pkg0/ProjetoAmbiental.java
@@ -10,16 +10,87 @@ public class ProjetoAmbiental {
     private String nome;
     private String descricao;
     private double orcamento;
-    List<AreaProtegida> ListAreaProtegida = new ArrayList<>();
-    List<RelatorioAmbiental> ListRelatorioAmbiental = new ArrayList<>();
-    List<Voluntario> ListVoluntario = new ArrayList<>();
-    List<Especie> ListEspecie = new ArrayList<>();
+    private ArrayList<AreaProtegida> ListAreaProtegida = new ArrayList<>();
+    private ArrayList<RelatorioAmbiental> ListRelatorioAmbiental = new ArrayList<>();
+    private ArrayList<Voluntario> ListVoluntario = new ArrayList<>();
+    private ArrayList<Especie> ListEspecie = new ArrayList<>();
 
     public ProjetoAmbiental(String nome, String descricao, double orcamento) {
         this.nome = nome;
         this.descricao = descricao;
         this.orcamento = orcamento;
     } 
+    
+    // metodo para adicionarArea(AreaProtegida area)
+    
+    public boolean adicionarArea(AreaProtegida area){
+        if (area != null){
+            ListAreaProtegida.add(area);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    // metodo para  listarAreas()
+    
+    public ArrayList<AreaProtegida> listarAreas(ArrayList<AreaProtegida> area){
+        return area;
+    }
+    
+    
+    // metodo para  adicionarRelatorio(RelatorioAmbiental relatorio)
+    
+    public boolean adicionarRelatorio(RelatorioAmbiental relatorio){
+        if (relatorio != null){
+            ListRelatorioAmbiental.add(relatorio);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    // metodo para  listarRelatorios() 
+    
+    public ArrayList<RelatorioAmbiental> listarRelatorios(ArrayList<RelatorioAmbiental> relatorio){
+        return relatorio;
+    }
+    
+    
+    // metodo para  adicionarVoluntario(Voluntario voluntario)
+    
+    public boolean  adicionarVoluntario(Voluntario voluntario){
+        if (voluntario != null){
+            ListVoluntario.add(voluntario);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    // metodo para  listarVoluntarios()
+    
+    public ArrayList<Voluntario> listarVoluntarios(ArrayList<Voluntario> voluntario){
+        return voluntario;
+    }
+    
+    // metodo para  adicionarEspecie(Especie especie)
+    
+    public boolean  adicionarEspecie(Especie especie){
+        if (especie != null){
+            ListEspecie.add(especie);
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+    // metodo para  listarEspecie()
+    
+    public ArrayList<Especie> listarEspecie(ArrayList<Especie> especie){
+        return especie;
+    }
+    
 
     public void exibirInformacoes() { 
         JOptionPane.showMessageDialog(null, "Nome: " + nome);
